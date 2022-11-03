e09462d131f792bcf3d402e706434c298f681c50
AdreReyes <108366142+AdreReyes@users.noreply.github.com>
2022-11-03 08:10:42 -0400

    changed cidrblock to 0.0.0.0/0 in assosication

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -8,26 +8,6 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "web_server01" {
-  ami                     = "ami-08c40ec9ead489470"
-  instance_type           = "t2.micro"
-  key_name                = "adrekeytoherheart"
-  subnet_id               = "${aws_subnet.prod-subnet-public-1.id}"
-  vpc_security_group_ids  = [aws_security_group.web_ssh.id]
-
-  user_data = "${file("deploy.sh")}"
-
-  tags = {
-    "Name" : "Webserver001"
-  }
-  
-}
-
-output "instance_ip" {
-  value = aws_instance.web_server01.public_ip
-  
-}
-
       resource "aws_vpc" "prod-vpc" {
        cidr_block           = "10.0.0.0/16"
        enable_dns_support   = "true" #gives you an internal domain name
@@ -43,36 +23,55 @@ output "instance_ip" {
      resource "aws_internet_gateway" "prod-igw" {
          vpc_id = "${aws_vpc.prod-vpc.id}"
          
-    tags = {
+         tags = {
          "Name" : "prod-igw"
      }
-     }
+}
 
      resource "aws_subnet" "prod-subnet-public-1" {
-    vpc_id                  = "${aws_vpc.prod-vpc.id}"
-    cidr_block              = "10.0.1.0/24"
-    map_public_ip_on_launch = "true" //it makes this a public subnet
-    availability_zone       = "us-east-1a"
-    tags = {
+      vpc_id                  = "${aws_vpc.prod-vpc.id}"
+      cidr_block              = "10.0.1.0/24"
+      map_public_ip_on_launch = "true" //it makes this a public subnet
+      availability_zone       = "us-east-1a"
+      
+      tags = {
       "Name" : "prod-subnet-public-1"
       }
-      }
+}
 
      resource "aws_route_table" "public-crt" {
-     vpc_id = "${aws_vpc.prod-vpc.id}"
+      vpc_id = "${aws_vpc.prod-vpc.id}"
      }
-
+     # Route table Association with Public Subnet's
      resource "aws_route_table_association" "crta-public-subnet-1"{
      subnet_id      = "${aws_subnet.prod-subnet-public-1.id}"
      route_table_id = "${aws_route_table.public-crt.id}"
-#       route {
- #        //associated subnet can reach everywhere
-  #       cidr_block = "0.0.0.0/16" 
-   #      //CRT uses this IGW to reach internet
-    #     gateway_id = "${aws_internet_gateway.prod-igw.id}" 
-     #}
-    #
-     # tags = {
-      #    "Name" : "public-crt"
-     #}
+       route {
+         //associated subnet can reach everywhere
+         cidr_block = "0.0.0.0/0" 
+         //CRT uses this IGW to reach internet
+         gateway_id = "${aws_internet_gateway.prod-igw.id}" 
      }
+      tags = {
+          "Name" : "public-crt"
+     }
+}
+
+     resource "aws_instance" "web_server01" {
+       ami                     = "ami-08c40ec9ead489470"
+       instance_type           = "t2.micro"
+       key_name                = "adrekeytoherheart"
+       subnet_id               = "${aws_subnet.prod-subnet-public-1.id}"
+       vpc_security_group_ids  = [aws_security_group.web_ssh.id]
+
+       user_data = "${file("deploy.sh")}"
+       
+       tags = {
+         "Name" : "Webserver001"
+  }
+  
+}
+     output "instance_ip" {
+       value = aws_instance.web_server01.public_ip
+  
+}
