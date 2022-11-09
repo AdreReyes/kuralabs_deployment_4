264bef78e0be09a7f5dd190179e1275accda7ade
AdreReyes <108366142+AdreReyes@users.noreply.github.com>
2022-11-09 18:23:10 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -7,10 +7,62 @@ provider "aws" {
   region = "us-east-1"
   
 }
+
+      resource "aws_vpc" "prod-vpc" {
+       cidr_block           = "10.0.0.0/16"
+       enable_dns_support   = "true" #gives you an internal domain name
+       enable_dns_hostnames = "true" #gives you an internal host name
+       instance_tenancy     = "default"    
+    
+    tags = {
+        "Name" : "prod-vpc"
+    }
+}
+
+
+     resource "aws_internet_gateway" "prod-igw" {
+         vpc_id = "${aws_vpc.prod-vpc.id}"
+         
+         tags = {
+         "Name" : "prod-igw"
+     }
+}
+
+     resource "aws_subnet" "prod-subnet-public-1" {
+      vpc_id                  = "${aws_vpc.prod-vpc.id}"
+      cidr_block              = "10.0.1.0/24"
+      map_public_ip_on_launch = "true" //it makes this a public subnet
+      availability_zone       = "us-east-1a"
+      
+      tags = {
+      "Name" : "prod-subnet-public-1"
+      }
+}
+
+     resource "aws_route_table" "public-crt" {
+      vpc_id = "${aws_vpc.prod-vpc.id}"
+       route {
+         //associated subnet can reach everywhere
+         cidr_block = "0.0.0.0/0" 
+         //CRT uses this IGW to reach internet
+         gateway_id = "${aws_internet_gateway.prod-igw.id}" 
+     }
+       tags = {
+          "Name" : "public-crt"
+     }
+}
+
+     # Route table Association with Public Subnet's
+     resource "aws_route_table_association" "crta-public-subnet-1"{
+       subnet_id      = "${aws_subnet.prod-subnet-public-1.id}"
+       route_table_id = "${aws_route_table.public-crt.id}"     
+}
+
      resource "aws_instance" "web_server01" {
        ami                     = "ami-08c40ec9ead489470"
        instance_type           = "t2.micro"
        key_name                = "adrekeytoherheart"
+       subnet_id               = "${aws_subnet.prod-subnet-public-1.id}"
        vpc_security_group_ids  = [aws_security_group.web_ssh.id]
 
        user_data = "${file("deploy.sh")}"
